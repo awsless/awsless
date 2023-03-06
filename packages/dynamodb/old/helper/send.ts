5e0a3ea00ec897a804ad2521bdfd2755809e3873
Ivan da Silveira <ivandasilveira@Ivans-MacBook-Pro.local>
2023-03-06 11:25:08 +0100

    changes

--- packages/dynamodb/old/helper/send.ts
+++
@@ -1,9 +0,0 @@
-
-import { Options } from '../types.js'
-import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb'
-import { dynamoDBDocumentClient } from '../client.js'
-
-export const send = (command:any, options:Options) => {
-	const client:DynamoDBDocumentClient = options.client || dynamoDBDocumentClient()
-	return client.send(command)
-}
