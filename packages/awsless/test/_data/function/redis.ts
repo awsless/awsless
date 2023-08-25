4be42ee3a0160a67bfa823039858a7339f2056c9
Michel Monen <michel@monen.com>
2023-08-25 14:33:05 +0200

    changes

--- packages/awsless/test/_data/function/redis.ts
+++ packages/awsless/test/_data/function/redis.ts
@@ -1,32 +1,19 @@
-import { getCacheProps } from "../../../src/node/resource";
+import { getCacheProps } from '../../../src/node/resource'
 import { command } from '../../../../redis/src/index'
 
 const props = getCacheProps('cache')
 
 export default async () => {
+	console.log(props)
 
-	console.log(props);
-
-	// await command(props, async (redis) => {
-
-	// 	// await redis.set('test', 'LOL')
-
-	// 	const value = await redis.get('test')
-
-	// 	console.log(value);
-
-	// 	return value
-	// })
-
-	await command(props, async (redis) => {
-
+	return await command(props, async redis => {
 		// await redis.set('test', 'LOL')
 
-		const value1 = await redis.get('test')
+		const value = await redis.get('test')
 		const value2 = await redis.get('test')
 
-		// console.log(value);
+		console.log(value)
 
-		// return value
+		return value
 	})
 }
