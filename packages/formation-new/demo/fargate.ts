f683b65299c35df71b9ccce56eeac3cf16d83168
weewoo <>
2025-07-25 11:52:52 +0200

    changes

---
+++ packages/formation-new/demo/fargate.ts
@@ -0,0 +1,56 @@
+import { homedir } from 'node:os'
+import { join } from 'node:path'
+import {
+	$,
+	App,
+	enableDebug,
+	FileLockBackend,
+	FileStateBackend,
+	Input,
+	Stack,
+	Terraform,
+	WorkSpace,
+} from '../src/index.ts'
+
+// enableDebug()
+
+const terraform = new Terraform({
+	providerLocation: join(homedir(), `.awsless/providers`),
+})
+
+const aws = await terraform.install('hashicorp', 'aws', '5.94.1')
+const dir = join(import.meta.dirname, 'build')
+
+const workspace = new WorkSpace({
+	providers: [
+		aws({
+			profile: 'jacksclub',
+			region: 'us-east-1',
+		}),
+		// archive({}),
+		// cloudFlare({}),
+	],
+	backend: {
+		state: new FileStateBackend({ dir }),
+		lock: new FileLockBackend({ dir }),
+	},
+})
+
+// ----------------------------------------
+
+const app = new App('app')
+const base = new Stack(app, 'base')
+
+const cluster = new $.aws.ecs.Cluster(base, 'cluster', {
+	name: 'fargate-cluster',
+})
+
+try {
+	console.log('Deploying Fargate resources...')
+
+	// await workspace.deploy(app)
+	await workspace.delete(app)
+} catch (error) {
+	console.log(error)
+	// throw error;
+}
