61f139bab8b2f6fd84c6912d870fa7afaaf167bd
Dhairya  Verma <dhairya@Dhairyas-MacBook-Pro.local>
2025-07-30 12:51:43 +0530

    changes

--- packages/awsless-next/cli/publish-fargate-image.sh
+++ packages/awsless-next/cli/publish-fargate-image.sh
@@ -1,3 +1,10 @@
-#!/usr/bin/env zsh
+#!/bin/bash
 
-# Build + Deploy the image to AWS ECR Public Repository
+region="us-east-1"
+reponame="nodejs/server"
+account="public.ecr.aws/d7g8v4v5"
+
+aws ecr-public get-login-password --region "$region" | docker login --username AWS --password-stdin "$account"
+docker build --platform=linux/amd64 -t "$reponame" ./fargate-image
+docker tag "$reponame:latest" "$account/$reponame:latest"
+docker push "$account/$reponame:latest"
