1a864ee76a7c12aa053f986efdce44abb724b187
Tanmay Banerjee <tanmay.banerjee23rd@gmail.com>
2026-01-05 18:40:17 +0530

    implemented bun secrets api for aws credentials

---
+++ packages/awsless-v3/src/cli/ui/complex/fetch-credentials.ts
@@ -0,0 +1,78 @@
+import { Cancelled, log, prompt } from '@awsless/clui'
+
+type StaticCredentials = {
+	accessKeyId: string
+	secretAccessKey: string
+}
+
+export const fetchCredentials = async (profile: string): Promise<StaticCredentials> => {
+	let accessKeyId = await Bun.secrets.get({
+		service: `${profile}-aws-credentials`,
+		name: 'access-key',
+	})
+	let secretAccessKey = await Bun.secrets.get({
+		service: `${profile}-aws-credentials`,
+		name: 'secret-key',
+	})
+
+	if (!accessKeyId || !secretAccessKey) {
+		accessKeyId = await prompt.password({
+			message: 'Enter your AWS access key ID',
+			mask: '*',
+			validate: (value: string) => {
+				const AWS_ACCESS_KEY_ID_REGEX = /^(AKIA|ASIA|AROA|AIDA)[A-Z0-9]{16}$/
+
+				if (!AWS_ACCESS_KEY_ID_REGEX.test(value)) {
+					log.error('Invalid AWS access key ID')
+					process.exit(1)
+				}
+
+				return undefined
+			},
+		})
+
+		log.success('Valid AWS access key ID')
+
+		secretAccessKey = await prompt.password({
+			message: 'Enter your AWS secret access key',
+			mask: '*',
+			validate: (value: string) => {
+				const AWS_SECRET_ACCESS_KEY_REGEX = /^[A-Za-z0-9/+=]{40}$/
+
+				if (!AWS_SECRET_ACCESS_KEY_REGEX.test(value)) {
+					log.error('Invalid AWS secret access key')
+					process.exit(1)
+				}
+
+				return undefined
+			},
+		})
+
+		log.success('Valid AWS secret access key')
+
+		log.task({
+			initialMessage: 'Saving AWS credentials in Bun secrets',
+			successMessage: 'AWS credentials saved successfully',
+			errorMessage: 'Failed to save AWS credentials',
+			task: async () => {
+				await Bun.secrets.set({
+					service: `${profile}-aws-credentials`,
+					name: 'access-key',
+					value: accessKeyId!,
+				})
+				await Bun.secrets.set({
+					service: `${profile}-aws-credentials`,
+					name: 'secret-key',
+					value: secretAccessKey!,
+				})
+			},
+		})
+
+		log.success('AWS credentials saved successfully')
+	}
+
+	return {
+		accessKeyId,
+		secretAccessKey,
+	}
+}
--- packages/awsless-v3/src/util/aws.ts
+++ packages/awsless-v3/src/util/aws.ts
@@ -1,12 +1,15 @@
 import { GetCallerIdentityCommand, STSClient } from '@aws-sdk/client-sts'
-import { createCredentialChain, fromEnv, fromIni } from '@aws-sdk/credential-providers'
+import { createCredentialChain } from '@aws-sdk/credential-providers'
 import { AwsCredentialIdentityProvider } from '@aws-sdk/types'
+import { fetchCredentials } from '../cli/ui/complex/fetch-credentials.js'
 import { Region } from '../config/schema/region.js'
 
 export type Credentials = AwsCredentialIdentityProvider
 
 export const getCredentials = (profile: string): Credentials => {
-	return createCredentialChain(fromIni({ profile }), fromEnv())
+	return createCredentialChain(async () => {
+		return await fetchCredentials(profile)
+	})
 }
 
 export const getAccountId = async (credentials: Credentials, region: Region): Promise<string> => {
