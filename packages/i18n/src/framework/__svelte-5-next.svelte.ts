435a6449c9a2a3d9841d96c7de976a554bf64dcc
Ivan <>
2026-01-08 21:18:39 +0100

    merge

--- packages/i18n/src/framework/__svelte-5-next.svelte.ts
+++ packages/i18n/src/framework/__svelte-5-next.svelte.ts
@@ -1,13 +1,21 @@
+import { type Snippet, createRawSnippet } from 'svelte'
+
 declare const $state: <T>(v: T) => T
 declare const $derived: { by: <T>(c: () => T) => T }
 
 type StringArgs = Array<string | number | { toString(): string }>
+type SnippetArgs = Array<Snippet | string | number | { toString(): string }>
 
 type Translate = {
 	(template: TemplateStringsArray, ...args: StringArgs): string
+	(template: TemplateStringsArray, ...args: SnippetArgs): Snippet
 }
 
 let locale = $state('en')
+
+const hasSnippet = (args: SnippetArgs | StringArgs) => {
+	return args.some(arg => typeof arg === 'function')
+}
 
 let t: Translate = $derived.by(() => {
 	const api = (template: TemplateStringsArray, ...args: StringArgs) => {
@@ -16,6 +24,31 @@ let t: Translate = $derived.by(() => {
 
 	api.get = (og: string, translations: Record<string, string>) => {
 		return translations[locale] ?? og
+	}
+
+	api.s = (template: TemplateStringsArray, ...args: StringArgs | SnippetArgs) => {
+		if (hasSnippet(args)) {
+			return createRawSnippet(() => ({
+				render: () => `<span>${`<span></span>`.repeat(template.length + args.length)}</span>`,
+				setup(root) {
+					const combined = template.flatMap((t, i) => [t, args[i]])
+
+					for (const i in combined) {
+						const value = combined[i]
+						const node = root.childNodes[i]
+						if (node && typeof value !== 'undefined') {
+							if (typeof value === 'function') {
+								value()(node)
+							} else {
+								node.textContent = value.toString()
+							}
+						}
+					}
+				},
+			}))
+		} else {
+			return String.raw({ raw: template.raw }, ...args)
+		}
 	}
 
 	return api
