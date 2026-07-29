1bd62f2443d3e4bc0def4ddd86ef57025653f379
Ivan <>
2026-07-29 16:26:13 +0200

    fix i18n bug

--- packages/i18n/src/find/typescript.ts
+++ packages/i18n/src/find/typescript.ts
@@ -4,9 +4,8 @@ import { simple } from 'swc-walk'
 export const findTypescriptTranslatable = async (code: string) => {
 	const found: string[] = []
 
-	const ast = await parse(code, {
-		syntax: 'typescript',
-	})
+	const ast = await parse(code, { syntax: 'typescript' })
+	const bytes = Buffer.from(code, 'utf8')
 
 	simple(ast, {
 		TaggedTemplateExpression(node: TaggedTemplateExpression) {
@@ -17,12 +16,21 @@ export const findTypescriptTranslatable = async (code: string) => {
 				node.tag.property.type === 'Identifier' &&
 				node.tag.property.value === 't'
 			) {
-				const content = code.substring(
-					node.template.span.start - ast.span.start + 1,
-					node.template.span.end - ast.span.start - 1
-				)
+				const content = bytes
+					.subarray(
+						node.template.span.start - ast.span.start + 1,
+						node.template.span.end - ast.span.start - 1
+					)
+					.toString('utf8')
 
 				found.push(content)
+
+				// const content = code.substring(
+				// 	node.template.span.start - ast.span.start + 1,
+				// 	node.template.span.end - ast.span.start - 1
+				// )
+
+				// found.push(content)
 			}
 		},
 	})
